52cae6e38af20c6d91419a5748ec6738c06427e8
Alexandr Prokofyev <a.a.prokofyev@mail.ru>
2016-09-27 21:45:16 +0300

    Fix navigation menu on top

--- src/app/pages/page-about-me/page-about-me__navigation.component.ts
+++ src/app/pages/page-about-me/page-about-me__navigation.component.ts
@@ -1,20 +1,32 @@
 import {
-    Component
+    Component,
+    AfterViewInit
 } from '@angular/core';
 
 import { IPagePart } from './page-about-me.types';
 import { PageAboutMeService } from './page-about-me.service';
 
-declare var $: JQueryStatic;
-
 @Component({
     selector: 'pa-page-about-me-navigation',
     template: require('./page-about-me__navigation.pug'),
-    styles: [ require('./page-about-me__navigation.scss') ]
+    styles: [require('./page-about-me__navigation.scss')]
 })
-export class PageAboutMeNavigationComponent {
+export class PageAboutMeNavigationComponent implements AfterViewInit {
+
+    navBarIsFixed: boolean = false;
+    navBarCollapsed: boolean = true;
 
     constructor(private pageAboutMeService: PageAboutMeService) {
+    }
+
+    ngAfterViewInit() {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > window.innerHeight) {
+                this.navBarIsFixed = true;
+            } else {
+                this.navBarIsFixed = false;
+            }
+        });
     }
 
     get pageParts(): IPagePart<Object>[] {
